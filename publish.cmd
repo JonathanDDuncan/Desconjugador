6d253eaca2bee01facd7915365ac75a6c191a0fa
Jonathan Duncan <duncanjonathan@yahoo.ca>
2017-01-26 13:38:37 -0600

    Adding published

---
+++ publish.cmd
@@ -0,0 +1,9 @@
+xcopy dist\*.* published /s /y /d
+rem Please commit any changes to the publish folder
+pause
+git branch -dr origin/gh-pages
+git subtree push --prefix published origin gh-pages
+
+pause
+rem published site is https://jonathandduncan.github.io/desconjugador/
+pause
